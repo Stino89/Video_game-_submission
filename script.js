191751130ec8449b093ff17f38cf9fa55d95a597
Stino89 <ftr1989@gmail.com>
2024-02-18 09:43:49 -0800

    index added

--- script.js
+++ script.js
@@ -49,6 +49,27 @@ window.addEventListener('load', function(){
 
     }
     class Particle {
+        constructor(game, x, y){
+            this.game = game;
+            this.x = x;
+            this.y = y;
+            this.image = document.getElementById('gears');
+            this.frameX =Math.floor(Math.random() * 3);
+            this.frameY =Math.floor(Math.random() * 3);
+            this.spriteSize = 50;
+            this.sizeModifier = (Math.random() * 0.5 + 0.5).tofixed(1);
+            this.size = this.spriteSize * this.sizeModifier
+            this.speedX = Math.random() * 6 - 3;
+            this.speedY = Math.random() * -15;
+            this.gravity = 0.5;
+            this.markedForDeletion = false;
+            this.angle = 0;
+            this.va = Math.random() * 0.2 - 0.1;
+        }
+        update(){
+            this.angle += this.va;
+
+        }
     }
     class Player {
         constructor(game){
