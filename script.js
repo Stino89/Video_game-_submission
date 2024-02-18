a6d5698758320a105c3528185ff0a7ecdf129e91
Stino89 <ftr1989@gmail.com>
2024-02-18 10:05:26 -0800

    2 to 5

--- script.js
+++ script.js
@@ -273,10 +273,10 @@ window.addEventListener('load', function(){
             super(game);
             this.width = 228;
             this.height = 169;
-            this.y = Math.random() * (this.game.height * 0.9 - this.height);
+            this.y = Math.random() * (this.game.height * 0.95 - this.height);
             this.image = document.getElementById('angler1');
             this.frameY = Math.floor(Math.random() * 3);
-            this.lives = 2;
+            this.lives = 5;
             this.score = this.lives;
         }
     }
