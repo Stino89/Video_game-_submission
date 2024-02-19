d3f33ef8d6260f1e0302e9e92de833cbf9c5f693
Stino89 <ftr1989@gmail.com>
2024-02-18 16:17:21 -0800

    lower t

--- script.js
+++ script.js
@@ -261,7 +261,7 @@ window.addEventListener('load', function(){
         }
         draw(context){
             if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
-           context.drawImage(This.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height,  this.x, this.y, this.width, this.height);
+           context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
            if (this.game.debug){
             context.font = '20px Helvetica';
             context.fillText(this.lives, this.x, this.y);
